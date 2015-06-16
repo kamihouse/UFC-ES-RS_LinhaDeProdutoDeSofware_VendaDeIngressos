803cf203530de35f6fbce7433827e1bc313024df
Thiago Pereira <thiagor@engineer.com>
2015-06-16 14:35:08 -0300

    Bugfix classe MakeAnOrder

--- componentes/PurchasingManagement/MakeAnOrder.php
+++ componentes/PurchasingManagement/MakeAnOrder.php
@@ -2,6 +2,7 @@
     require_once "../../sistema/SetupComponente.php";
 
     $sc = new SetupComponente();
+    $metodo = isset($_GET['metodo']) ? $_GET['metodo'] : null;
 
 	if(!isset($_SESSION)){
 		session_start();
@@ -39,8 +40,11 @@
                         <div class="col-xs-6">
                             <div class="row col-xs-7">
                                 <?php
+                                    echo($metodo == "Boleto Bancario") ? '&metoco=bb' : '';
+                                ?>
+                                <?php
                                     foreach($pm->metodosPagamento as $metodo){
-                                        echo '<a href="'. base_url() .'componentes/PurchasingManagement/PurchasingManagement.php?acao=compraRealizada" class="btn btn-block btn-lg btn-inverse">'. $metodo .'</a>';
+                                        echo '<a href="'. base_url() .'componentes/PurchasingManagement/PurchasingManagement.php?acao=compraRealizada'. ($metodo == "Boleto Bancario" ? '&metodo=bb' : '') .'" class="btn btn-block btn-lg btn-inverse">'. $metodo .'</a>';
                                     }
                                 ?>
                             </div>
@@ -56,7 +60,15 @@
 			$sessao = isset($_SESSION['nome']) ? $_SESSION['nome'] : null;
 
 			global $sc;
-			$sc->pprint('head');
+
+            global $metodo;
+
+            $sc->pprint('head');
+
+            if(!isset($sessao) AND $metodo != 'bb'){
+                $this->exibeTelaSolLogin();
+                die();
+            }
 			?>
 			<div class="container">
 				<div class="logo">
@@ -92,4 +104,43 @@
 			<?php
 			$sc->pprint('loadjs');
 		}
+
+
+        public function exibeTelaSolLogin(){
+            global $sc;
+
+
+            $sessao = isset($_SESSION['nome']) ? $_SESSION['nome'] : null;
+
+            $sc->pprint('head');
+            ?>
+            <div class="container">
+                <div class="logo">
+                    <h1>UFC RS - LPS<span>Venda de Ingressos</span><dt>2.0</dt></h1>
+                </div>
+
+                <div class="col-xs-12">
+                    <?php $sc->pprint('row_menu'); ?>
+                    <div class="row geral">
+                        <h4>Compra de Ingressos</h4>
+                        <p>
+                            Venda de Ingressos Online
+                        </p>
+                        <hr>
+
+                        <div class="col-xs-12">
+                            <p>
+                                <b><span class="fui-alert-circle"></span> Atenção!</b><br>
+                                Você precisa estar logado para realizar esta operação.</p>
+
+                            <div class="row col-xs-4">
+                                <a href="<?= $sc->base_url ?>componentes/Profile/Profile.php?acao=login" class="btn btn-block btn-lg btn-default">Efeturar Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+            $sc->pprint('loadjs');
+        }
 	}
